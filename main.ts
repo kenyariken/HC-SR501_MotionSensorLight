6d0d55a5729753d32b2d471bcf3f9a4dd6fecbc6
Ken <161325958+kenyariken@users.noreply.github.com>
2025-08-02 22:57:24 +0900

    LEDを全て点灯に変更

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        // 動きを検知したら表示
-        basic.showIcon(IconNames.Heart)
-        basic.pause(5000)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     } else {
         // 検知なしならLEDを消す
         basic.clearScreen()
